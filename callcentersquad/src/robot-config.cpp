60747874b4624bbaae258eb83573a8d110ee5e5e
Soham Solanki <sohamsolanki@outlook.com>
2021-12-23 15:34:39 -0600

    Added other template from MOGO

---
+++ callcentersquad/src/robot-config.cpp
@@ -0,0 +1,29 @@
+#include "vex.h"
+
+using namespace vex;
+using signature = vision::signature;
+using code = vision::code;
+
+// A global instance of brain used for printing to the V5 Brain screen
+brain  Brain;
+
+// VEXcode device constructors
+motor LeftBack = motor(PORT13, ratio18_1, false);
+motor RightBack = motor(PORT14, ratio18_1, false);
+motor tilter = motor(PORT15, ratio18_1, false);
+motor intake = motor(PORT17, ratio18_1, false);
+motor conveyor = motor(PORT12, ratio18_1, false);
+controller Controller1 = controller(primary);
+
+// VEXcode generated functions
+// define variable for remote controller enable/disable
+bool RemoteControlCodeEnabled = true;
+
+/**
+ * Used to initialize code/tasks/devices added using tools in VEXcode Pro.
+ * 
+ * This should be called at the start of your int main function.
+ */
+void vexcodeInit( void ) {
+  // nothing to initialize
+}
