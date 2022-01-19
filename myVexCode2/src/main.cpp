29e4f231b38099ef92164aebb8e61f1b545ca5eb
Soham Solanki <sohamsolanki@outlook.com>
2022-01-19 07:30:25 -0600

    Updated code

--- myVexCode2/src/main.cpp
+++ myVexCode2/src/main.cpp
@@ -25,44 +25,20 @@ using namespace vex;
 // A global instance of competition
 competition Competition;
 
+
 // define your global instances of motors and other devices here
 
-/*---------------------------------------------------------------------------*/
-/*                          Pre-Autonomous Functions                         */
-/*                                                                           */
-/*  You may want to perform some actions before the competition starts.      */
-/*  Do them in the following function.  You must return from this function   */
-/*  or the autonomous and usercontrol tasks will not be started.  This       */
-/*  function is only called once after the V5 has been powered on and        */
-/*  not every time that the robot is disabled.                               */
-/*---------------------------------------------------------------------------*/
-
-
+// PRE-AUTONOMOUS CODE
 
 void pre_auton(void) {
   // Initializing Robot Configuration. DO NOT REMOVE!
   vexcodeInit();
-
-  // All activities that occur before the competition starts
-  // Example: clearing encoders, setting servo positions, ...
-  Brain.Screen.print("Begin");
 }
 
-/*---------------------------------------------------------------------------*/
-/*                                                                           */
-/*                              Autonomous Task                              */
-/*                                                                           */
-/*  This task is used to control your robot during the autonomous phase of   */
-/*  a VEX Competition.                                                       */
-/*                                                                           */
-/*  You must modify the code to add your own robot specific commands here.   */
-/*---------------------------------------------------------------------------*/
+// AUTONOMOUS CODE
 
 void autonomous(void) {
-  // ..........................................................................
-  // Insert autonomous user code here.
-  // ..........................................................................
-  // My autonomous code below should drive the robot forward 6 feet, pick up a mobile goal, and then drive back.
+  // GOAL: My autonomous code below should drive the robot forward 6 feet, pick up a mobile goal, and then drive back.
   Forklift.setVelocity(100,percent);
  
   Brain.Screen.print("Hello World!");
@@ -78,27 +54,12 @@ void autonomous(void) {
   
 }
 
-/*---------------------------------------------------------------------------*/
-/*                                                                           */
-/*                              User Control Task                            */
-/*                                                                           */
-/*  This task is used to control your robot during the user control phase of */
-/*  a VEX Competition.                                                       */
-/*                                                                           */
-/*  You must modify the code to add your own robot specific commands here.   */
-/*---------------------------------------------------------------------------*/
+// DRIVER CONTROL
 
 void usercontrol(void) {
   // User control code here, inside the loop
   while (1) {
-    // This is the main execution loop for the user control program.
-    // Each time through the loop your program should update motor + servo
-    // values based on feedback from the joysticks.
 
-    // ........................................................................
-    // Insert user code here. This is where you use the joystick values to
-    // update your motors, etc.
-    // ........................................................................
     Brain.Screen.print("Finished");
 
     Drivetrain.setDriveVelocity(100,percent);
@@ -107,8 +68,7 @@ void usercontrol(void) {
     Forklift.setVelocity(100,percent);
  
 
-    wait(20, msec); // Sleep the task for a short amount of time to
-                    // prevent wasted resources.
+    wait(20, msec);                
   }
 }
 
